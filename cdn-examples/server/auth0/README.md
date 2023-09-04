a4e7824bb6680ef7d89640ac6ca4dd57ba4e9ea0
Matt Bailey <m.bailey@arkoselabs.com>
2023-09-04 09:25:57 +0100

    Cookie-Form as standard.

--- cdn-examples/server/auth0/README.md
+++ cdn-examples/server/auth0/README.md
@@ -1,16 +1,20 @@
 # Auth0 - Cloudflare Worker proxy
 
-This is an example of a Cloudflare worker that can be setup as a proxy with Auth0 integrations both injecting client side script and handling the verification on the CDN level. This worker would be setup to work as a proxy for Auth0 login requests, on the GET based login screen for Auth0 this proxy would inject javascript into the page that would hijack the form submission and load an Arkose challenge on submission. On the POST based login request this worker would intercept the request, extract the Arkose token from a definied cookie and verify the token. On successful verification the request would continue, on failed verification the request would redirect to a defined login page.
+This is an example of a Cloudflare worker that can be setup as a proxy with Auth0 integrations both injecting client side script and handling the verification on the CDN level. This worker would be setup to work as a proxy for Auth0 requests, on the GET based login screen for Auth0 this proxy would inject javascript into the page that would hijack the form submission and load an Arkose challenge on submission. On the POST based request this worker would intercept the request, extract the Arkose token from a definied cookie and verify the token. On successful verification the request would continue, on failed verification the request would redirect to a defined login page.
 
 ## Configuration
 This worker includes several environment variables that can be setup to customise the behaviour of the worker.
 
-| Variable            | Description                                                                          | Default                                |
-| ------------------- | ------------------------------------------------------------------------------------ | -------------------------------------- |
-| publicKey           | The Arkose public key to use in this worker                                          | `11111111-1111-1111-1111-111111111111` |
-| privateKey          | The Arkose private key to use in this worker                                         | ``                                     |
-| errorUrl            | A url to redirect error states to if required                                        | `https://arkoselabs.com`               |
-| cookieName          | The name of the cookie that the token will be passed in                              | `arkose-token`                         |
-| failOpen            | A boolean to indicate if we should fail open or not                                  | `true`                                 |
-| verifyMaxRetryCount | The number of times to retry verification if there is an error                       | `3`                                    |
-| scriptMaxRetryCount | The number of times to retry resetting Arkose if there is a client side error        | `3`                                    |
+| Variable              | Description                                                                          | Default                                |
+| --------------------- | ------------------------------------------------------------------------------------ | -------------------------------------- |
+| publicKey             | The Arkose public key to use in this worker, avaliable via the portal                | `11111111-1111-1111-1111-111111111111` |
+| privateKey            | The Arkose private key to use in this worker, avaliable via the portal               | `11111111-1111-1111-1111-111111111111` |
+| clientSubdomain       | Client subdomain name provided to you via the Arkose Labs team                       | `client-api`                           |
+| verifySubdomain       | Verify subdomain name provided to you via Arkose Labs team                           | `verify-api`                           |
+| errorUrl              | A page to route the user to when an error occurs (outside of fail open)              | `"https://www.arkoselabs.com"`         |
+| arkoseCookieLife      | The length of time the Arkose cookies should be active for (in milliseconds)         | `30000 (5 minutes)`                    |
+| arkoseCookieName      | The name of the cookie that the Arkose token will be stored in                       | `arkoseToken`                          |
+| arkoseErrorCookieName | The name of the cookie that an Arkose error will be stored in                        | `arkoseError`                          |
+| scriptMaxRetryCount   | The number of times to retry resetting Arkose if there is a client side error        | `3`                                    |
+| verifyMaxRetryCount   | The number of times to retry verification if there is an error                       | `3`                                    |
+| failOpen              | A boolean to indicate if we should fail open or not in case of outage                | `true`                                 |
--- cdn-examples/server/auth0/index.js
+++ cdn-examples/server/auth0/index.js
@@ -9,13 +9,15 @@
  * @param {string} clientSubdomain A customer's specific subdomain used for the loading of the Client-API (if setup)
  * @param {string} verifySubdomain A customer's specific subdomain used for the verification call (if setup)
  * @param {string} errorUrl A url to redirect to if there has been an error
- * @param {string} cookieName The name of the cookie to store the Arkose Labs session token in
- * @param {string} failOpen A boolean string to indicate if the current session should fail
- * open if there is a problem with the Arkose Labs platform.
+ * @param {string} arkoseCookieLife  The length of time the Arkose cookies should be active for (in milliseconds) 
+|* @param {string} arkoseCookieName The name of the cookie that the Arkose token will be stored in
+|* @param {string} arkoseErrorCookieName The name of the cookie that an Arkose error will be stored in
  * @param {string} verifyMaxRetryCount A numeric string to represent the number of times we should retry
  * Arkose Labs verification if there is an issue.
  * @param {string} scriptMaxRetryCount A numeric string to represent the number of times we should retry
  * loading an Arkose Labs challenge if there is an issue.
+ * * @param {string} failOpen A boolean string to indicate if the current session should fail
+ * open if there is a problem with the Arkose Labs platform.
  */
 
 /**
@@ -76,7 +78,7 @@ const checkArkoseStatus = async () => {
  * @param  {string} token The Arkose Labs session token value
  * @param  {string} privateKey The Arkose Labs private key
  * @param  {string} verifySubdomain The subdomain of the verify endpoint to use
- * @param  {string} retryMaxCount The number of retries that should be performed if there is an issue
+ * @param  {string} verifyMaxRetryCount The number of retries that should be performed if there is an issue
  * @param  {string} [currentRetry=0] The count of the current number of retries being performed
  * @return {Object} status The current verification and Arkose Labs platform status
  * @return {boolean} status.verified Has the token verified successfully
@@ -86,7 +88,7 @@ const verifyArkoseToken = async (
   token,
   privateKey,
   verifySubdomain,
-  retryMaxCount,
+  verifyMaxRetryCount,
   currentRetry = 0
 ) => {
   let verified = false;
@@ -111,14 +113,14 @@ const verifyArkoseToken = async (
   } catch {
     arkoseStatus = await checkArkoseStatus();
     if (arkoseStatus) {
-      if (currentRetry === retryMaxCount) {
+      if (currentRetry === verifyMaxRetryCount) {
         return { verified, arkoseStatus };
       }
       return verifyArkoseToken(
         token,
         privateKey,
         verifySubdomain,
-        retryMaxCount,
+        verifyMaxRetryCount,
         currentRetry + 1
       );
     }
@@ -140,10 +142,11 @@ export default {
     const { publicKey } = env;
     const { privateKey } = env;
     const { clientSubdomain = 'client-api' } = env;
-    const { verifySubdomain = 'client-api' } = env;
+    const { verifySubdomain = 'verify-api' } = env;
     const { errorUrl } = env;
-    const { cookieName = 'arkose-token' } = env;
-    const formSelector = '._form-login-password';
+    const { arkoseCookieName = 'arkoseToken' } = env;
+    const { arkoseErrorCookieName = 'arkoseError' } = env;
+    const arkoseCookieLife = parseNumber(env.arkoseCookieLife);
     const failOpen = parseBoolean(env.failOpen);
     const verifyMaxRetryCount = parseNumber(env.verifyMaxRetryCount);
     const scriptMaxRetryCount = parseNumber(env.scriptMaxRetryCount);
@@ -153,117 +156,151 @@ export default {
      * @param  {Object} response The current response to inject code into
      * @return  {Object} the modified response
      */
+    
     const injectArkose = (response) => {
       const newResponse = new HTMLRewriter()
         .on('body', {
           element(element) {
             element.append(
               `<script>
-var publicKey = '${publicKey}';
-var clientSubdomain = '${clientSubdomain}';
-var errorUrl = '${errorUrl}';
-var cookieName = '${cookieName}';
-var formSelector = '${formSelector}';
-var failOpen = ${failOpen};
-var arkoseRetryMax = ${scriptMaxRetryCount};
-var arkoseScriptSrc = 'https://' + clientSubdomain + '.arkoselabs.com/v2/' + publicKey + '/api.js';
+              var publicKey = '${publicKey}';
+              var clientSubdomain = '${clientSubdomain}';
+              var errorUrl = '${errorUrl}';
+              var arkoseCookieName = '${arkoseCookieName}';
+              var arkoseErrorCookieName = '${arkoseErrorCookieName}';
+              var arkoseCookieLife = '${arkoseCookieLife}';
+              var failOpen = ${failOpen};
+              var arkoseRetryMax = ${scriptMaxRetryCount};
+              var arkoseScriptSrc = 'https://' + clientSubdomain + '.arkoselabs.com/v2/' + publicKey + '/api.js';
+              
+              var arkose = null;
+              var arkoseRetry = 0;
+              var arkoseReady = false;
+              var arkoseResetting = false;
+              var arkoseCompleted = false;
+              var submitForm = document.querySelector('form');
+              var submitButton = null;
 
-var arkose = null;
-var arkoseRetry = 0;
-var arkoseReady = false;
-var arkoseResetting = false;
-var arkoseCompleted = false;
-var loginForm = document.querySelector(formSelector);
-var loginButton = null;
+              setupForm();
+      
+              /**
+              * Sets up the events for hijacking the form submission
+              */
+              function setupForm() {
+                  if (submitForm) {
+                      submitButton = submitForm.querySelector('[type=submit]');
+                      arkoseComplete = false;
+                      if (!arkoseReady) {
+                          submitButton.setAttribute('disabled', true);
+                      }
+                      submitForm.addEventListener('submit', function (event) {
+                          if (!arkoseReady) {
+                              event.preventDefault();
+                              return;
+                          }
+                          if (!arkoseComplete) {
+                              event.preventDefault();
+                              arkose.run();
+                              return;
+                          }
+                      })
+                  }
+              }
+      
+              /**
+              * Checks the current status of the Arkose Labs platform and invokes a callback with the result
+              * @param  {function} callback The function to call once the status check has been performed
+              */
+              function checkArkoseStatus(callback) {
+                  try {
+                      var xhr = new XMLHttpRequest();
+                      xhr.open('GET', 'https://status.arkoselabs.com/api/v2/status.json', false);
+                      xhr.onreadystatechange = function() {
+                          if (xhr.readyState == XMLHttpRequest.DONE) {
+                              if (this.status == 200) {
+                                  var res = JSON.parse(xhr.responseText);
+                                  var status = res.status.indicator;
+                                  callback(!(status === 'critical'));
+                                  return;
+                              }
+                              callback(false);
+                          }
+                      };
+                      xhr.send(null);
+                  } catch (error) {
+                      callback(false);
+                  }
+              }
+      
+              /**
+              * In the case of an error reset the arkose cookie and setup a new cookie that tracks the error
+              * @param  {string} error The error string to set in the error cookie
+              */
+              function handleError(error) {
+                  arkoseComplete = true;
+                  document.cookie = arkoseCookieName + '=;expires=' + new Date(Date.now() + arkoseCookieLife).toUTCString() + '; path=/;';
+                  document.cookie = arkoseErrorCookieName + '=' + error + ';expires=' + new Date(Date.now() + arkoseCookieLife).toUTCString() + '; path=/;';                               
+              }  
+              
+               /**
+              * The callback thats called after the Arkose Labs script has been loaded
+              * @param  {object} myEnforcement The Arkose Labs enforcement object
+              */
+              function setupEnforcement(myEnforcement) {
+                  arkose = myEnforcement;
+                  arkose.setConfig({
+                      onReady: function() {
+                          arkoseReady = true;
+                          if (submitButton) {
+                              submitButton.removeAttribute('disabled');
+                          }
+                          if (arkoseResetting) {
+                              arkoseResetting = false;
+                              arkose.run();
+                          }
+                          document.cookie = arkoseCookieName + '=' + '=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+                          document.cookie = arkoseErrorCookieName + '=' + '=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+                      },
+                      onCompleted: function(response) {
+                          arkoseComplete = true;
+                          if (response.token){
+                              document.cookie = arkoseCookieName + '=' + response.token + ';expires=' + new Date(Date.now() + arkoseCookieLife).toUTCString() + '; path=/;';
+                          } else {
+                              handleError('TOKEN_MISSING');
+                          }
+                          submitForm.submit();
+                      },
+                      onError: function(response) {
+                          checkArkoseStatus(function (isHealthy) {    
+                              if (isHealthy && arkoseRetry < arkoseMaxRetryCount) {
+                                  arkoseReady = false;
+                                  arkoseResetting = true;
+                                  arkose.reset();
+                                  arkoseRetry = arkoseRetry + 1;
+                                  return;
+                              }
+                              handleError(response.error ? response.error.error : 'error');
+                              submitButton.removeAttribute('disabled');
+                              submitForm.submit();
+                          });
+                      }
+                  });
+              }
 
-if (loginForm) {
-    loginButton = loginForm.querySelector('[type="submit"]');
-    loginButton.setAttribute('disabled', true);
-    loginForm.addEventListener('submit', function (event) {
-        if (!arkoseCompleted) {
-            event.preventDefault();
-            arkose.run();
-            return;
-        }
-        if (!arkoseReady) {
-            event.preventDefault();
-            return;
-        }
-    });
-}
-
-function createArkoseScript() {
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = arkoseScriptSrc;
-    script.setAttribute('data-callback', 'setupEnforcement');
-    script.async = true;
-    script.defer = true;
-    script.id = 'arkose-script';
-    document.getElementsByTagName('head')[0].appendChild(script);
-}
-
-function checkArkoseStatus(callback) {
-    try {
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', 'https://status.arkoselabs.com/api/v2/status.json', false);
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState == XMLHttpRequest.DONE) {
-                if (this.status == 200) {
-                    var res = JSON.parse(xhr.responseText);
-                    var status = res.status.indicator;
-                    callback(!(status === 'critical'));
-                    return;
-                }
-                callback(false);
+              function createArkoseScript() {
+                var script = document.createElement('script');
+                script.type = 'text/javascript';
+                script.src = arkoseScriptSrc;
+                script.setAttribute('data-callback', 'setupEnforcement');
+                script.async = true;
+                script.defer = true;
+                script.id = 'arkose-script';
+                document.getElementsByTagName('head')[0].appendChild(script);
             }
-        };
-        xhr.send(null);
-    } catch (error) {
-        callback(false);
-    }
-}
-  
-function setupEnforcement(enforcement) {
-    arkose = enforcement;
-    arkose.setConfig({
-        onReady: function() {
-            arkoseReady = true;
-            loginButton.removeAttribute('disabled');
-            document.cookie = cookieName + '=' + '""' + ';expires=' + new Date(Date.now() - 1).toUTCString() + ';path=/';
-            if (arkoseResetting) {
-                arkoseResetting = false;
-                arkose.run();
-            }
-        },
-        onCompleted: function(response) {
-            var cookieValue = response.token;
-            document.cookie = cookieName + '=' + cookieValue + ';expires=' + new Date(Date.now() + 60 * 5 * 1000).toUTCString() + ';path=/';
-            arkoseCompleted = true;
-            loginForm.submit();
-        },
-        onError: function(response) {
-            checkArkoseStatus(function (isHealthy) {
-                if (isHealthy && arkoseRetry < arkoseRetryMax) {
-                    arkoseResetting = true;
-                    arkose.reset();
-                    arkoseRetry = arkoseRetry + 1;
-                    return;
-                }
-                if (failOpen) {
-                    arkoseCompleted = true;
-                    arkoseReady = true;
-                    loginButton.removeAttribute('disabled');
-                    loginForm.submit();
-                    return;
-                }
-                window.location.replace(errorUrl);
-            });
-        },
-    });
-}
-createArkoseScript();
-</script>`,
+
+            createArkoseScript();
+
+          </script>`,
               { html: true }
             );
           },
@@ -290,7 +327,7 @@ createArkoseScript();
       return injectArkose(res);
     }
     // If the request is not a GET request and has an Arkose Labs session cookie, process it
-    const arkoseToken = getCookie(request.headers.get('Cookie'), cookieName);
+    const arkoseToken = getCookie(request.headers.get('Cookie'), arkoseCookieName);
     if (arkoseToken && arkoseToken !== '') {
       const verifyStatus = await verifyArkoseToken(
         arkoseToken,
